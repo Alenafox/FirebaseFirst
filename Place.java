9fef6307a84d1c2e6ce0ce66bb06d95fbeb3036e
Alenafox <57696470+Alenafox@users.noreply.github.com>
2021-05-22 15:09:01 +0800

    Create Place.java

---
+++ Place.java
@@ -0,0 +1,18 @@
+package com.example.myfirebase;
+
+public class Place {
+    public String name;
+    public double lat, lon;
+
+    public Place() {
+        // для использования с Firebase требуется конструктор по умолчанию
+    }
+    public Place(String name, double lat, double lon) {
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon;
+    }
+    public String toString () {
+        return "name: " + name + ", latitude: " + lat + ", longtitude: " + lon;
+    }
+}
